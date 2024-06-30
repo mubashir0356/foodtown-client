c17e718e74740bf0c00023733dd5bf5f77b027be
Abdul Mubashir <mubashir0356@gmail.com>
2024-06-30 09:30:27 +0530

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-slate-800 w-screen h-screen text-red-500 text-center">
+      hello
+    </div>
+  );
+}
+
+export default App;
